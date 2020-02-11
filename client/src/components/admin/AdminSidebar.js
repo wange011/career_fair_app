1394f0634551d4319118f94e6cf70a5f720018b9
Fidel Flores <fefcaceres@gmail.com>
2020-02-11 13:49:46 -0500

    fixed styling

--- client/src/components/admin/AdminSidebar.js
+++ client/src/components/admin/AdminSidebar.js
@@ -4,18 +4,19 @@ import { connect } from 'react-redux';
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
+import './AdminSidebar.css';
 
 
 const items = [
     { name: 'home', label: 'Home' },
-    { name: 'billing', label: 'Billing' },
+    { name: 'analytics', label: 'Analytics' },
     { name: 'settings', label: 'Settings' },
 ]
 
 
 function AdminSidebar() {
     return (
-        <div className="adminSidebar col-lg-2 col-md-2 col-sm-2">
+        <div className="AdminSideBar col-lg-2 col-md-2 col-sm-2">
 
             <List disablePadding dense>
                 {items.map(({ label, name, ...rest }) => (
